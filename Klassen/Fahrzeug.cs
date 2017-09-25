0d235a92a3628bb625b9ca0c59e90bb6141e4110
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-09-25 17:06:53 +0200

    Klassen & Properties

---
+++ Klassen/Fahrzeug.cs
@@ -0,0 +1,57 @@
+﻿namespace Klassen
+{
+    internal class Fahrzeug
+    {
+        private int geschwindigkeit;     // Feld
+        public int Geschwindigkeit
+        {
+            get
+            {
+                return geschwindigkeit;
+            }
+            set
+            {
+                if (value > 200)
+                    value = 200;
+                 
+                geschwindigkeit = value;
+            }
+        }
+
+        #region Java Version von Datenkapselung
+        //public int GetGeschwindigkeit()
+        //{
+        //    return geschwindigkeit;
+        //}
+        //public void SetGeschwindigkeit(int v)
+        //{
+        //    if (v > 200)
+        //        v = 200;
+
+        //    geschwindigkeit = v;
+        //}
+        #endregion
+
+        private string farbe;
+        public string Farbe
+        {
+            get { return farbe; }
+            set
+            {
+                switch (value)
+                {
+                    case "Rot":
+                    case "Blau":
+                    case "Grün":
+                    case "Gelb":
+                        farbe = value;
+                        break;
+                    default:
+                        farbe = "Ungültig";
+                        break;
+                }
+            }
+        }
+
+    }
+}
---
+++ Klassen/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Klassen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fahrzeug pkw = new Fahrzeug();
+            pkw.Geschwindigkeit = 50;
+            pkw.Farbe = "Blau";
+
+            Fahrzeug bus = new Fahrzeug();
+            bus.Geschwindigkeit = int.MaxValue;
+            bus.Farbe = "Blau";
+
+            Console.WriteLine(bus.Geschwindigkeit);
+
+            Console.ReadKey();
+        }
+    }
+}
