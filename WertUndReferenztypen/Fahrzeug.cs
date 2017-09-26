25cb2da7fbf5cf09b7a667153efc0509154a88bb
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-09-26 13:15:54 +0200

    Wert und Referenztypen

---
+++ WertUndReferenztypen/Fahrzeug.cs
@@ -0,0 +1,20 @@
+﻿namespace WertUndReferenztypen
+{
+    internal class Fahrzeug
+    {
+        private int geschwindigkeit;
+        public int Geschwindigkeit
+        {
+            get { return geschwindigkeit; }
+            set { geschwindigkeit = value; }
+        }
+
+        public Fahrzeug Copy()
+        {
+            return new Fahrzeug
+            {
+                Geschwindigkeit = this.Geschwindigkeit
+            };
+        }
+    }
+}
---
+++ WertUndReferenztypen/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace WertUndReferenztypen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Objekte von Klassen sind immer Referenztypen
+            // Objekte von Strukturen sind immer Werttypen
+            // Ausnahme: class String -> hat das Verhalten eines Werttypes
+            
+            Console.WriteLine("------ Wert Typen ------");
+
+            int i = 5;
+            int j = i;
+
+            Console.WriteLine($"I: {i}");
+            Console.WriteLine($"J: {j}");
+
+            j = 100;
+
+            Console.WriteLine("\nJ ändern");
+            Console.WriteLine($"I: {i}");
+            Console.WriteLine($"J: {j}");
+
+
+            Console.WriteLine("\n\n\n");
+
+
+
+            Console.WriteLine("------ Refrenz Typen ------");
+
+            Fahrzeug pkw = new Fahrzeug();
+            pkw.Geschwindigkeit = 50;
+            Fahrzeug lkw = pkw;
+            //Fahrzeug lkw = new Fahrzeug();
+            //lkw.Geschwindigkeit = pkw.Geschwindigkeit;
+
+            Console.WriteLine($"Pkw: {pkw.Geschwindigkeit}");
+            Console.WriteLine($"Lkw: {lkw.Geschwindigkeit}");
+
+            lkw.Geschwindigkeit = 120;
+
+            Console.WriteLine("\nLkw beschleunigen");
+            Console.WriteLine($"Pkw: {pkw.Geschwindigkeit}");
+            Console.WriteLine($"Lkw: {lkw.Geschwindigkeit}");
+
+            Console.ReadKey();
+        }
+    }
+}
