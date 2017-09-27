bd59a239b865bb559e0a598d41235a83d7a1fba0
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-09-27 13:09:10 +0200

    Strukturen

---
+++ Strukturen/Fahrzeug.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Strukturen
+{
+    internal struct Fahrzeug : IDisposable
+    {
+        public Fahrzeug(int geschwindigkeit)
+        {
+            this.geschwindigkeit = geschwindigkeit;
+        }
+
+        private int geschwindigkeit;
+        public int Geschwindigkeit
+        {
+            get { return geschwindigkeit; }
+            set { geschwindigkeit = value; }
+        }
+
+        public void Beschleunigen(int dV) => Geschwindigkeit += dV;
+
+        public void Dispose()
+        { /* Das Fahrzeug zerstören. */ }
+    }
+}
---
+++ Strukturen/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Strukturen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new Fahrzeug(50);
+            
+            var pkw = f;        // bei strukturen eine echte Kopie -> Werttypen!!
+          
+
+            // Objekte von Strukturen werden am Stack gespeichert
+            // Objeket von Klassen am Heap
+
+            // Bei Strukturen gibts keine Vererbung
+        }
+    }
+}
