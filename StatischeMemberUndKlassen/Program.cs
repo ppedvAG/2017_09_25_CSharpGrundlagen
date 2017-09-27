88c7027503d151d06908f489af35af1838bb64ab
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-09-27 13:27:47 +0200

    Statische Member und Klassen

---
+++ StatischeMemberUndKlassen/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StatischeMemberUndKlassen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var r = new Rechner();
+            //var sub = r.Sub(9, 20);
+
+            var ergebnis = Rechner.Add(5, 9);
+        }
+    }
+}
---
+++ StatischeMemberUndKlassen/Rechner.cs
@@ -0,0 +1,18 @@
+﻿namespace StatischeMemberUndKlassen
+{
+    // NICHT Statische Klassen können statische und nicht statische Member beinhalten.
+    // Statische Klassen können nur statisch Member beinhalten.
+
+    internal static class Rechner
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
