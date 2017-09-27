51ff3634a80a4e11439c98870f66ad38aa2b2218
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-09-27 16:05:28 +0200

    Delegates, Anonyme Methoden und Lambda Expressions

---
+++ DelegatesInPraxis/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DelegatesInPraxis
+{
+    internal delegate bool MyDelegate(Employee e);
+    // Action       -> void
+    // Predicate<T> -> bool
+    // Func
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = GetData();
+
+            //MyDelegate del = new MyDelegate(Bedingung);
+            //Func<Employee, bool> del = new Func<Employee, bool>(Bedingung);
+            //var del = new Func<Employee, bool>(Bedingung);
+            //Func<Employee, bool> del = Bedingung;
+            //var query = Abfrage(employees, del);
+
+            //var query = Abfrage(employees, Bedingung);
+
+            //var query = Abfrage(employees, delegate (Employee e)
+            //{
+            //    return e.Experience < 5;
+            //});
+
+            //var query = Abfrage(employees, (Employee e) =>
+            //{
+            //    return e.Experience < 5;
+            //});
+
+            //var query = Abfrage(employees, (e) =>
+            //{
+            //    return e.Experience < 5;
+            //});
+
+            var query = Abfrage(employees, (e) => e.Experience < 5);
+
+            var linq = employees.Where(e => e.Experience > 5);
+            
+            foreach (var e in query)
+                Console.WriteLine($"Id: {e.Id,2} | {e.Name,-10} | {e.Experience}");
+            
+            Console.ReadKey();
+        }
+
+        private static bool Bedingung(Employee e)
+        {
+            return e.Name.StartsWith("s", StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private static IEnumerable<Employee> Abfrage(
+            IEnumerable<Employee> employees, 
+            Func<Employee, bool> predicate)
+        {
+            var query = new List<Employee>();
+
+            foreach (var e in employees)
+                if (predicate(e))
+                    query.Add(e);
+
+            return query;
+        }
+
+
+
+
+        private static IEnumerable<Employee> GetData()
+        {
+            return new[]
+            {
+                new Employee { Id = 1, Name = "Sepp", Experience = 5},
+                new Employee { Id = 2, Name = "Maria", Experience = 8},
+                new Employee { Id = 3, Name = "Stanislaus", Experience = 3},
+                new Employee { Id = 4, Name = "Lisa", Experience = 9},
+                new Employee { Id = 5, Name = "Anna", Experience = 1},
+                new Employee { Id = 6, Name = "Thomas", Experience = 6},
+                new Employee { Id = 7, Name = "Hannes", Experience = 5},
+                new Employee { Id = 8, Name = "Max", Experience = 8},
+                new Employee { Id = 9, Name = "Luise", Experience = 2}
+            };
+        }
+    }
+}
---
+++ HalloDelegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HalloDelegates
+{
+    public delegate string MyDelegate(int zahl, double wert);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate del = new MyDelegate(MeineMethode);
+
+            var result = del.Invoke(8, 9.3);
+            System.Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        private static string MeineMethode(int i, double d)
+        {
+            return (i + d).ToString();
+        }
+    }
+}
