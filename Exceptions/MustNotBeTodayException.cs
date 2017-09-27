2b79cfd4316bb6f1d09b9fcd4f1935a5b6f2f999
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-09-27 11:58:21 +0200

    Exceptions

---
+++ Exceptions/MustNotBeTodayException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exceptions
+{
+    internal class MustNotBeTodayException : Exception
+    {
+        public MustNotBeTodayException(int forbiddenNumber, string message) : base(message)
+        {
+            ForbiddenNumber = forbiddenNumber;
+        }
+
+        public int ForbiddenNumber { get; }
+    }
+}
---
+++ Exceptions/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int zahl1 = int.Parse(Console.ReadLine());
+                int zahl2 = int.Parse(Console.ReadLine());
+
+                var today = DateTime.Now.Day;
+                if (zahl1 == today || zahl2 == today)
+                    throw new MustNotBeTodayException(today, "Heute ist bei mir ganz schlecht. Probiers mal morgen.");
+
+                int ergebnis = checked(zahl1 + zahl2);
+
+                Console.WriteLine(ergebnis);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("So nicht!");
+                Console.WriteLine(ex.Message);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Bitte kleinere Zahlen eingeben.");
+            }
+            catch(MustNotBeTodayException ex) when (ex.ForbiddenNumber > 10)
+            {
+                Console.WriteLine($"Gößer 10");
+                Console.WriteLine($"Die verbotene Zahl: {ex.ForbiddenNumber}");
+                Console.WriteLine(ex.Message);
+            }
+            catch (MustNotBeTodayException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                // todo
+            }
+            finally
+            {
+                Console.WriteLine("Wird immer ausgeführt!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
